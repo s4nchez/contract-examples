e27bbb88ec4d5cb57e83a8e9363e5fd512e3fddc
Ivan Sanchez <ivan@gourame.com>
2023-08-15 21:16:51 +0100

    Rename test class

--- pokemon-cli/src/test/kotlin/org/example/PokemonClientFakeServerTest.kt
+++ pokemon-cli/src/test/kotlin/org/example/PokemonClientFakeServerTest.kt
@@ -4,7 +4,7 @@ import org.http4k.core.*
 import org.http4k.format.Jackson.auto
 
 
-class PokemonClientTest : PokemonClientContract() {
+class PokemonClientFakeServerTest : PokemonClientContract() {
     private val fakeServer = { _: Request ->
         val data = Body.auto<PokemonDetails>().toLens()
         Response(Status.OK).with(data of PokemonDetails("pikachu", 4))
