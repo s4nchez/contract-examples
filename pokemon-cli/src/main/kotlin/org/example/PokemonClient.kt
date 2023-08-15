537e4734d7e19eb62ebe5dec2d54b9f8f61dc824
Ivan Sanchez <ivan@gourame.com>
2023-08-15 19:57:54 +0100

    Fake implementation

--- pokemon-cli/src/main/kotlin/org/example/PokemonClient.kt
+++ pokemon-cli/src/main/kotlin/org/example/PokemonClient.kt
@@ -2,9 +2,7 @@ package org.example
 
 class PokemonClient {
 
-    fun fetch(pokemonName: String): PokemonDetails {
-        TODO("Not yet implemented")
-    }
+    fun fetch(pokemonName: String): PokemonDetails = PokemonDetails("pikachu", 4)
 }
 
 data class PokemonDetails(val name: String, val height: Int)
